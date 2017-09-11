6b46d2b5d2311e6e5751758572effd85b279b76f
imolina2014 <imolina2014@alu.uct.cl>
2017-09-11 01:12:25 -0300

    ACTUALIZADO INDEX Y FUNCIONAMIENTO

--- js/funcionamiento.js
+++ js/funcionamiento.js
@@ -2,18 +2,11 @@ var TextoLeido;
 var Postulantes = [];
 var Aspirantes  = [];
 var ConTeorico  = [];
-var EvaTeorivo  = [];
 var ConPractico = [];
-var EvaPractico = [];
 var Brigadistas = [];
+var Reprobados = []; 
 
 //Rut1,Nombre1,Nombre2,Apellido1,Apellido2,Edad,FechaNac,Celular;
-
-
-
-
-
-
 
 //Se lee el texto y se deja su contenido en la variable TextoLeido
 function processFiles(files) {
@@ -55,26 +48,232 @@ function procesamiento() {
 			var checkbox = document.createElement('br');
 			checkbox1.appendChild(checkbox);
 			cont = 0;
-		}
-		
+		}	
 	}
-
 }
 
-function recuperar_check(){
-	
-	for (var i = Postulantes.length - 1; i >= 0; i--) {
 
-		var porId=document.getElementById(Postulantes[i][0]).checked;
-		if (porId==true){
-			alert("Esta chekeado");
-			
 
+
+
+
+
+
+// function push_array1(){
+// 	for (var i = Postulantes.length - 1; i >= 0; i--) {
+// 		var porId=document.getElementById(Postulantes[i][0]).checked;
+// 		if (porId==true){
+// 			Aspirantes.push(Postulantes[i]);
+// 		}else{
+// 			Reprobados.push(Postulantes[i]);
+// 		}
+// 	}
+// 	muestra_checks1();
+// }
+// function muestra_checks1(recibe){
+// 	cont = 0;
+// 	for (var i = Aspirantes.length - 1; i >= 0; i--) {  
+// 		var checkbox = document.createElement('input');
+// 		checkbox.type = "checkbox";
+// 		checkbox.name = Aspirantes[i][0];
+// 		checkbox.value = Aspirantes[i][0];
+// 		checkbox.id = Aspirantes[i][0]+1;    
+// 		checkbox2.appendChild(checkbox);
+// 		var checkbox = document.createTextNode(Aspirantes[i][1]+" "+Aspirantes[i][3]+" "+Aspirantes[i][4]);
+// 		checkbox2.appendChild(checkbox);
+// 		cont++;
+// 		if (cont==5){
+// 			var checkbox = document.createElement('br');
+// 			checkbox2.appendChild(checkbox);
+// 			cont = 0;
+// 		}	
+// 	}
+// }
+
+
+
+
+
+
+
+// function push_array2(){
+// 	for (var i = Aspirantes.length - 1; i >= 0; i--) {
+// 		var porId=document.getElementById(Aspirantes[i][0]+1).checked;
+// 		if (porId==true){
+// 			ConTeorico.push(Aspirantes[i]);
+// 		}else{
+// 			Reprobados.push(Aspirantes[i]);
+// 		}
+// 	}
+
+// 	muestra_checks2();
+// }
+// function muestra_checks2(recibe){
+// 	cont = 0;
+// 	for (var i = ConTeorico.length - 1; i >= 0; i--) {  
+// 		var checkbox = document.createElement('input');
+// 		checkbox.type = "checkbox";
+// 		checkbox.name = ConTeorico[i][0];
+// 		checkbox.value = ConTeorico[i][0];
+// 		checkbox.id = ConTeorico[i][0]+2;    
+// 		checkbox3.appendChild(checkbox);
+// 		var checkbox = document.createTextNode(ConTeorico[i][1]+" "+ConTeorico[i][3]+" "+ConTeorico[i][4]);
+// 		checkbox3.appendChild(checkbox);
+// 		cont++;
+// 		if (cont==5){
+// 			var checkbox = document.createElement('br');
+// 			checkbox3.appendChild(checkbox);
+// 			cont = 0;
+// 		}	
+// 	}
+// }
+
+
+
+
+
+
+
+
+
+// function push_array3(){
+// 	for (var i = ConTeorico.length - 1; i >= 0; i--) {
+// 		var porId=document.getElementById(ConTeorico[i][0]+2).checked;
+// 		if (porId==true){
+// 			ConPractico.push(ConTeorico[i]);
+// 		}else{
+// 			Reprobados.push(ConTeorico[i]);
+// 		}
+// 	}
+
+// 	muestra_checks3();
+// }
+// function muestra_checks3(recibe){
+// 	cont = 0;
+// 	for (var i = ConPractico.length - 1; i >= 0; i--) {  
+// 		var checkbox = document.createElement('input');
+// 		checkbox.type = "checkbox";
+// 		checkbox.name = ConPractico[i][0];
+// 		checkbox.value = ConPractico[i][0];
+// 		checkbox.id = ConPractico[i][0]+3;    
+// 		checkbox4.appendChild(checkbox);
+// 		var checkbox = document.createTextNode(ConPractico[i][1]+" "+ConPractico[i][3]+" "+ConPractico[i][4]);
+// 		checkbox4.appendChild(checkbox);
+// 		cont++;
+// 		if (cont==5){
+// 			var checkbox = document.createElement('br');
+// 			checkbox4.appendChild(checkbox);
+// 			cont = 0;
+// 		}	
+// 	}
+// }
+
+
+
+
+
+
+
+// function push_array4(){
+// 	for (var i = ConPractico.length - 1; i >= 0; i--) {
+// 		var porId=document.getElementById(ConPractico[i][0]+3).checked;
+// 		if (porId==true){
+// 			Brigadistas.push(ConPractico[i]);
+// 		}else{
+// 			Reprobados.push(ConPractico[i]);
+// 		}
+// 	}
+
+// 	muestra_checks4();
+// }
+// function muestra_checks4(recibe){
+// 	for (var i = Brigadistas.length - 1; i >= 0; i--) {  
+// 		var checkbox = document.createTextNode(Brigadistas[i]);
+// 		checkbox5.appendChild(checkbox);
+// 		var checkbox = document.createElement('br');
+// 			checkbox5.appendChild(checkbox);
+// 	}
+// 	alert(Reprobados);
+// }
+
+
+
+
+
+function push_array(paso){   //PASO HABLA DEL PASO DEL HTML (POSTULANTE, ASPIRANTE, ETC.)
+	if (paso==1){
+		a= Postulantes;				//ESTA PARTE ES PARA VER QUE ARREGLO PROCESAREMOS Y CUAL LLENAREMOS RESPECTIVAMENTE
+		b= Aspirantes;
+	}
+	if (paso==2){
+		a= Aspirantes;
+		b= ConTeorico;
+	}
+	if (paso==3){
+		a= ConTeorico;
+		b= ConPractico;
+	}
+	if (paso==4){
+		a= ConPractico;
+		b= Brigadistas;
+	}
+	for (var i = a.length - 1; i >= 0; i--) {
+		if (paso==1)var porId=document.getElementById(a[i][0]).checked;   //POR UNA MAÑA DE ABAJO SE DEBIERON PONER ESTOS IF y ELSE
+		else {
+			num = paso -1;
+			n = num.toString();
+			var porId=document.getElementById(a[i][0]+n).checked;
+		}
+
+		if (porId==true){												//SI ESTA CLICKEADO SE HACE PUSH AL ARREGLO SGTE. SI NO SE HACE PUSH A LOS REPROBADOS
+			b.push(a[i]);
 		}else{
-			alert("No esta chekeado");
+			Reprobados.push(a[i]);
 		}
 	}
-
-
-
+	muestra_checks(paso);											//PASAMOS A LA FUNCION DE MAS ABAJO
+}
+function muestra_checks(paso){   
+	cont = 0;														// ESTO ES SOLO PARA VER CUANTOS SE MOSTRA POR LINEA
+	if (paso==1){
+		a= Aspirantes;
+		b= checkbox2;
+	}
+	if (paso==2){													//LO MISMO DE ARRIBA. PARA VER QUE ARREGLO SE LEERA PARA CREAR LOS CHECKBOX
+		a= ConTeorico;
+		b= checkbox3;												//Y B ES PAARA NO REPETIR LAS ETIQUETAS DE HTML(PUDO SER MAS OPTIMIZADO)
+	}
+	if (paso==3){
+		a= ConPractico;
+		b= checkbox4;
+	}
+	if (paso==4){													//SOLO EN CASO DE QUE SEA EL ULTIMO PASO SE MOSTRARAN TODOS LOS DATOS DE LOS BRIGADISTAS FINALES
+		a= Brigadistas;
+		for (var i = Brigadistas.length - 1; i >= 0; i--) {  
+			var checkbox = document.createTextNode(Brigadistas[i]);
+			checkbox5.appendChild(checkbox);
+			var checkbox = document.createElement('br');
+				checkbox5.appendChild(checkbox);
+		}
+		alert(Reprobados);
+	}
+	if (paso==1 || paso==2 || paso==3 ){							//EN CASO DE QUE NO SEA EL PASO FINAL SE CREAN CHECKBOX CON LOS ASPIRANTES QUE VA AVANZANDO.
+		alert("Entra")
+		for (var i = a.length - 1; i >= 0; i--) {  
+			var checkbox = document.createElement('input');
+			checkbox.type = "checkbox";
+			checkbox.name = a[i][0];
+			checkbox.value = a[i][0];
+			checkbox.id = a[i][0]+paso;    
+			b.appendChild(checkbox);
+			var checkbox = document.createTextNode(a[i][1]+" "+a[i][3]+" "+a[i][4]);
+			b.appendChild(checkbox);
+			cont++;
+			if (cont==5){
+				var checkbox = document.createElement('br');
+				b.appendChild(checkbox);
+				cont = 0;
+			}	
+		}
+	}
 }
